856ca5e33ba8723b6154355e0913e666d2a8a5c6
John Grun <john.grun@gmail.com>
2017-10-18 20:57:36 -0400

    Added the rest of the screens from the report. Non of the screens in this commit do anything and are not linked to anywhere or from anywhere

---
+++ 1_Software/napit/src/components/Exercise/Exercise.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, Image, View, KeyboardAvoidingView } from 'react-native';
+
+
+export default class Exercise extends Component {
+  render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Text style={styles.title}> NapIT </Text>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  logo: {
+    width:100,
+    height:100
+  },
+  title: {
+    color: '#FFF',
+    marginTop: 10,
+    width: 160,
+    textAlign: 'center',
+    opacity: 0.9
+  }
+});
