d688a03658a2fd6628208df8ab45726aa4872bd9
John Grun <john.grun@gmail.com>
2017-10-19 20:20:03 -0400

    Added new routes to work with the NapIt App

--- 1_Software/Databse_Manager/router.js
+++ 1_Software/Databse_Manager/router.js
@@ -6,9 +6,31 @@ import { index } from './controllers/movies';
 const router = Router();
 
 // Handle /movies.json route with index action from movies controller
-router.route('/movies.json')
-  .get(index);
+//router.route('/movies.json')
+  //.get(index);
+router.get('/movies.json',index);
+//Account actions
+router.get('/User', function (req, res) 
+{
+	res.send('GET request /USER')
+});
 
-  //router.route('/AddUser').get();
+router.post('/User', function (req, res) 
+{
+	res.send('Post request /USER')
+});
+
+router.get('/Sensor', function (req, res) 
+{
+	res.send('GET request /Sensor')
+});
+
+router.post('/Sensor', function (req, res) 
+{
+	res.send('POST request /Sensor')
+});
+// Sensors
+//router.route('/SensorReadings').post();
+
 
 export default router;
