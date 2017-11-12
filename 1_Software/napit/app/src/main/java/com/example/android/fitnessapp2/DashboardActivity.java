aa10874dfe6125279efc397f4e6c45c4f8611243
John Grun <john.grun@gmail.com>
2017-11-12 12:35:30 -0500

    Attempting to add the android version

---
+++ 1_Software/napit/app/src/main/java/com/example/android/fitnessapp2/DashboardActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.fitnessapp2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DashboardActivity extends AppCompatActivity {
+
+ImageButton sleepImage, exerciseImage, foodImage, vsImage, sugImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        sleepImage=(ImageButton) findViewById(R.id.sleepicon);
+        exerciseImage=(ImageButton) findViewById(R.id.exerciseicon);
+        foodImage=(ImageButton) findViewById(R.id.foodicon);
+        vsImage=(ImageButton)findViewById(R.id.vsicon);
+        sugImage=(ImageButton) findViewById(R.id.sugicon);
+        sleepImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              Intent i = new Intent(DashboardActivity.this,SleepActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+}
---
+++ 1_Software/napit/app/src/main/java/com/example/android/fitnessapp2/SleepActivity.java
@@ -0,0 +1,83 @@
+package com.example.android.fitnessapp2;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class SleepActivity extends Activity implements SensorEventListener {
+    private TextView xText, yText, zText;
+    private Sensor mySensor;
+
+    private SensorManager SM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep);
+        // Create our Sensor Manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        // Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Register sensor Listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        // Assign TextView
+        xText = (TextView)findViewById(R.id.xText);
+        yText = (TextView)findViewById(R.id.yText);
+        zText = (TextView)findViewById(R.id.zText);
+
+        // series = new LineGraphSeries<DataPoint>();
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not in use
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        xText.setText("X: " + event.values[0]);
+        yText.setText("Y: " + event.values[1]);
+        zText.setText("Z: " + event.values[2]);
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+}
